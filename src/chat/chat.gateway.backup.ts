97782da3e56d00451108c06682a82832f9e43c24
r0lm0 <rolmo92@gmail.com>
2025-10-18 05:06:52 -0600

    Update pingTimeout in AuthSocket configuration for improved WebSocket performance

--- src/chat/chat.gateway.backup.ts
+++ src/chat/chat.gateway.backup.ts
@@ -31,7 +31,7 @@ interface AuthSocket extends Socket {
     },
     namespace: '/chat',
     transports: ['websocket'],
-    pingTimeout: 60000,        // antes 20000
+    pingTimeout: 120000,        // antes 20000
     pingInterval: 25000,       // default ok
     perMessageDeflate: false,  // MUY importante con proxys
 })
